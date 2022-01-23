18847d270d094092278715f312f7b7073bdf54b4
Justin Dasilva <61707143+jdsilv17@users.noreply.github.com>
2022-01-23 11:10:40 -0500

    Removed VR and Touch related class members from CubeShifterCharacter class

--- CubeShifter/Source/CubeShifter/CubeShifterCharacter.cpp
+++ CubeShifter/Source/CubeShifter/CubeShifterCharacter.cpp
@@ -9,8 +9,6 @@
 #include "GameFramework/InputSettings.h"
 #include "HeadMountedDisplayFunctionLibrary.h"
 #include "Kismet/GameplayStatics.h"
-#include "MotionControllerComponent.h"
-#include "XRMotionControllerBase.h" // for FXRMotionControllerBase::RightHandSourceId
 
 DEFINE_LOG_CATEGORY_STATIC(LogFPChar, Warning, All);
 
@@ -59,29 +57,29 @@ ACubeShifterCharacter::ACubeShifterCharacter()
 	// Note: The ProjectileClass and the skeletal mesh/anim blueprints for Mesh1P, FP_Gun, and VR_Gun 
 	// are set in the derived blueprint asset named MyCharacter to avoid direct content references in C++.
 
-	// Create VR Controllers.
-	R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
-	R_MotionController->MotionSource = FXRMotionControllerBase::RightHandSourceId;
-	R_MotionController->SetupAttachment(RootComponent);
-	L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
-	L_MotionController->SetupAttachment(RootComponent);
+	//// Create VR Controllers.
+	//R_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("R_MotionController"));
+	//R_MotionController->MotionSource = FXRMotionControllerBase::RightHandSourceId;
+	//R_MotionController->SetupAttachment(RootComponent);
+	//L_MotionController = CreateDefaultSubobject<UMotionControllerComponent>(TEXT("L_MotionController"));
+	//L_MotionController->SetupAttachment(RootComponent);
 
-	// Create a gun and attach it to the right-hand VR controller.
-	// Create a gun mesh component
-	VR_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VR_Gun"));
-	VR_Gun->SetOnlyOwnerSee(false);			// otherwise won't be visible in the multiplayer
-	VR_Gun->bCastDynamicShadow = false;
-	VR_Gun->CastShadow = false;
-	VR_Gun->SetupAttachment(R_MotionController);
-	VR_Gun->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
+	//// Create a gun and attach it to the right-hand VR controller.
+	//// Create a gun mesh component
+	//VR_Gun = CreateDefaultSubobject<USkeletalMeshComponent>(TEXT("VR_Gun"));
+	//VR_Gun->SetOnlyOwnerSee(false);			// otherwise won't be visible in the multiplayer
+	//VR_Gun->bCastDynamicShadow = false;
+	//VR_Gun->CastShadow = false;
+	//VR_Gun->SetupAttachment(R_MotionController);
+	//VR_Gun->SetRelativeRotation(FRotator(0.0f, -90.0f, 0.0f));
 
-	VR_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("VR_MuzzleLocation"));
-	VR_MuzzleLocation->SetupAttachment(VR_Gun);
-	VR_MuzzleLocation->SetRelativeLocation(FVector(0.000004, 53.999992, 10.000000));
-	VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));		// Counteract the rotation of the VR gun model.
+	//VR_MuzzleLocation = CreateDefaultSubobject<USceneComponent>(TEXT("VR_MuzzleLocation"));
+	//VR_MuzzleLocation->SetupAttachment(VR_Gun);
+	//VR_MuzzleLocation->SetRelativeLocation(FVector(0.000004, 53.999992, 10.000000));
+	//VR_MuzzleLocation->SetRelativeRotation(FRotator(0.0f, 90.0f, 0.0f));		// Counteract the rotation of the VR gun model.
 
-	// Uncomment the following line to turn motion controllers on by default:
-	//bUsingMotionControllers = true;
+	//// Uncomment the following line to turn motion controllers on by default:
+	////bUsingMotionControllers = true;
 }
 
 void ACubeShifterCharacter::BeginPlay()
@@ -92,17 +90,17 @@ void ACubeShifterCharacter::BeginPlay()
 	//Attach gun mesh component to Skeleton, doing it here because the skeleton is not yet created in the constructor
 	FP_Gun->AttachToComponent(Mesh1P, FAttachmentTransformRules(EAttachmentRule::SnapToTarget, true), TEXT("GripPoint"));
 
-	// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
-	if (bUsingMotionControllers)
-	{
-		VR_Gun->SetHiddenInGame(false, true);
-		Mesh1P->SetHiddenInGame(true, true);
-	}
-	else
-	{
-		VR_Gun->SetHiddenInGame(true, true);
-		Mesh1P->SetHiddenInGame(false, true);
-	}
+	//// Show or hide the two versions of the gun based on whether or not we're using motion controllers.
+	//if (bUsingMotionControllers)
+	//{
+	//	VR_Gun->SetHiddenInGame(false, true);
+	//	Mesh1P->SetHiddenInGame(true, true);
+	//}
+	//else
+	//{
+	//	VR_Gun->SetHiddenInGame(true, true);
+	//	Mesh1P->SetHiddenInGame(false, true);
+	//}
 }
 
 //////////////////////////////////////////////////////////////////////////
@@ -122,9 +120,9 @@ void ACubeShifterCharacter::SetupPlayerInputComponent(class UInputComponent* Pla
 	PlayerInputComponent->BindAction("AltFire", IE_Pressed, this, &ACubeShifterCharacter::OnAltFire);
 
 	// Enable touchscreen input
-	EnableTouchscreenMovement(PlayerInputComponent);
+	//EnableTouchscreenMovement(PlayerInputComponent);
 
-	PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ACubeShifterCharacter::OnResetVR);
+	//PlayerInputComponent->BindAction("ResetVR", IE_Pressed, this, &ACubeShifterCharacter::OnResetVR);
 
 	// Bind movement events
 	PlayerInputComponent->BindAxis("MoveForward", this, &ACubeShifterCharacter::MoveForward);
@@ -147,13 +145,13 @@ void ACubeShifterCharacter::OnFire()
 		UWorld* const World = GetWorld();
 		if (World != nullptr)
 		{
-			if (bUsingMotionControllers)
-			{
-				const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
-				const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
-				World->SpawnActor<ACubeShifterProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
-			}
-			else
+			//if (bUsingMotionControllers)
+			//{
+			//	const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
+			//	const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
+			//	World->SpawnActor<ACubeShifterProjectile>(ProjectileClass, SpawnLocation, SpawnRotation);
+			//}
+			//else
 			{
 				const FRotator SpawnRotation = GetControlRotation();
 				// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
@@ -194,13 +192,13 @@ void ACubeShifterCharacter::OnAltFire()
 		UWorld* const World = GetWorld();
 		if (World != nullptr)
 		{
-			if (bUsingMotionControllers)
-			{
-				const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
-				const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
-				World->SpawnActor<ACubeShifterProjectile>(AltProjectileClass, SpawnLocation, SpawnRotation);
-			}
-			else
+			//if (bUsingMotionControllers)
+			//{
+			//	const FRotator SpawnRotation = VR_MuzzleLocation->GetComponentRotation();
+			//	const FVector SpawnLocation = VR_MuzzleLocation->GetComponentLocation();
+			//	World->SpawnActor<ACubeShifterProjectile>(AltProjectileClass, SpawnLocation, SpawnRotation);
+			//}
+			//else
 			{
 				const FRotator SpawnRotation = GetControlRotation();
 				// MuzzleOffset is in camera space, so transform it to world space before offsetting from the character location to find the final muzzle position
@@ -234,35 +232,35 @@ void ACubeShifterCharacter::OnAltFire()
 	}
 }
 
-void ACubeShifterCharacter::OnResetVR()
-{
-	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
-}
+//void ACubeShifterCharacter::OnResetVR()
+//{
+//	UHeadMountedDisplayFunctionLibrary::ResetOrientationAndPosition();
+//}
 
-void ACubeShifterCharacter::BeginTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
-{
-	if (TouchItem.bIsPressed == true)
-	{
-		return;
-	}
-	if ((FingerIndex == TouchItem.FingerIndex) && (TouchItem.bMoved == false))
-	{
-		OnFire();
-	}
-	TouchItem.bIsPressed = true;
-	TouchItem.FingerIndex = FingerIndex;
-	TouchItem.Location = Location;
-	TouchItem.bMoved = false;
-}
-
-void ACubeShifterCharacter::EndTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
-{
-	if (TouchItem.bIsPressed == false)
-	{
-		return;
-	}
-	TouchItem.bIsPressed = false;
-}
+//void ACubeShifterCharacter::BeginTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
+//{
+//	if (TouchItem.bIsPressed == true)
+//	{
+//		return;
+//	}
+//	if ((FingerIndex == TouchItem.FingerIndex) && (TouchItem.bMoved == false))
+//	{
+//		OnFire();
+//	}
+//	TouchItem.bIsPressed = true;
+//	TouchItem.FingerIndex = FingerIndex;
+//	TouchItem.Location = Location;
+//	TouchItem.bMoved = false;
+//}
+//
+//void ACubeShifterCharacter::EndTouch(const ETouchIndex::Type FingerIndex, const FVector Location)
+//{
+//	if (TouchItem.bIsPressed == false)
+//	{
+//		return;
+//	}
+//	TouchItem.bIsPressed = false;
+//}
 
 //Commenting this section out to be consistent with FPS BP template.
 //This allows the user to turn without using the right virtual joystick
@@ -332,17 +330,17 @@ void ACubeShifterCharacter::LookUpAtRate(float Rate)
 	AddControllerPitchInput(Rate * BaseLookUpRate * GetWorld()->GetDeltaSeconds());
 }
 
-bool ACubeShifterCharacter::EnableTouchscreenMovement(class UInputComponent* PlayerInputComponent)
-{
-	if (FPlatformMisc::SupportsTouchInput() || GetDefault<UInputSettings>()->bUseMouseForTouch)
-	{
-		PlayerInputComponent->BindTouch(EInputEvent::IE_Pressed, this, &ACubeShifterCharacter::BeginTouch);
-		PlayerInputComponent->BindTouch(EInputEvent::IE_Released, this, &ACubeShifterCharacter::EndTouch);
-
-		//Commenting this out to be more consistent with FPS BP template.
-		//PlayerInputComponent->BindTouch(EInputEvent::IE_Repeat, this, &ACubeShifterCharacter::TouchUpdate);
-		return true;
-	}
-	
-	return false;
-}
+//bool ACubeShifterCharacter::EnableTouchscreenMovement(class UInputComponent* PlayerInputComponent)
+//{
+//	if (FPlatformMisc::SupportsTouchInput() || GetDefault<UInputSettings>()->bUseMouseForTouch)
+//	{
+//		PlayerInputComponent->BindTouch(EInputEvent::IE_Pressed, this, &ACubeShifterCharacter::BeginTouch);
+//		PlayerInputComponent->BindTouch(EInputEvent::IE_Released, this, &ACubeShifterCharacter::EndTouch);
+//
+//		//Commenting this out to be more consistent with FPS BP template.
+//		//PlayerInputComponent->BindTouch(EInputEvent::IE_Repeat, this, &ACubeShifterCharacter::TouchUpdate);
+//		return true;
+//	}
+//	
+//	return false;
+//}
