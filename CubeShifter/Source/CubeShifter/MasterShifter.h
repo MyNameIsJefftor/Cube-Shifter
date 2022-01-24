53a4d56796ef5187a795b8e0d11463a97544e849
Justin Dasilva <61707143+jdsilv17@users.noreply.github.com>
2022-01-23 19:51:36 -0500

    Create MasterShifter cpp class

---
+++ CubeShifter/Source/CubeShifter/MasterShifter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Shiftable.h"
+#include "MasterShifter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CUBESHIFTER_API AMasterShifter : public AShiftable
+{
+	GENERATED_BODY()
+	
+};
