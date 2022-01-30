00cb0863d8710433b57f59fd2542b7a9cc43bcab
Justin Dasilva <61707143+jdsilv17@users.noreply.github.com>
2022-01-30 13:11:25 -0500

    Replaced Crosshair

--- CubeShifter/Source/CubeShifter/CubeShifterHUD.cpp
+++ CubeShifter/Source/CubeShifter/CubeShifterHUD.cpp
@@ -10,7 +10,7 @@
 ACubeShifterHUD::ACubeShifterHUD()
 {
 	// Set the crosshair texture
-	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/FirstPersonCrosshair"));
+	static ConstructorHelpers::FObjectFinder<UTexture2D> CrosshairTexObj(TEXT("/Game/FirstPerson/Textures/circle-crosshair-png.circle-crosshair-png"/*"/Game/FirstPerson/Textures/FirstPersonCrosshair"*/));
 	CrosshairTex = CrosshairTexObj.Object;
 }
 
@@ -25,11 +25,13 @@ void ACubeShifterHUD::DrawHUD()
 	const FVector2D Center(Canvas->ClipX * 0.5f, Canvas->ClipY * 0.5f);
 
 	// offset by half the texture's dimensions so that the center of the texture aligns with the center of the Canvas
-	const FVector2D CrosshairDrawPosition( (Center.X),
-										   (Center.Y + 20.0f));
+	const FVector2D CrosshairDrawPosition( (Center.X - 24.0f),
+										   (Center.Y));
+
+	DrawTexture(CrosshairTex, CrosshairDrawPosition.X, CrosshairDrawPosition.Y, 64.0f, 64.0f, 0.0f, 0.0f, 1.0f, 1.0f);
 
 	// draw the crosshair
-	FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
-	TileItem.BlendMode = SE_BLEND_Translucent;
-	Canvas->DrawItem( TileItem );
+	//FCanvasTileItem TileItem( CrosshairDrawPosition, CrosshairTex->Resource, FLinearColor::White);
+	//TileItem.BlendMode = SE_BLEND_Translucent;
+	//Canvas->DrawItem( TileItem );
 }
